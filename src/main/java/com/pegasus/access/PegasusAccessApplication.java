4231abd95b06f301581c6fd2fb0d22d58675fd2f
carlosdev29 <caluss48@hotmail.com>
2025-06-28 16:58:07 -0500

    first-module

---
+++ src/main/java/com/pegasus/access/PegasusAccessApplication.java
@@ -0,0 +1,13 @@
+package com.pegasus.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegasusAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegasusAccessApplication.class, args);
+	}
+
+}
